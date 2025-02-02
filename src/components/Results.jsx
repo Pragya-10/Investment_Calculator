0e8858ec38b08447d5cc6af3fa5af23fc4070655
Pragya-10 <pragyamishra10501@gmail.com>
2025-02-02 23:59:03 +0530

    Working on UI

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,6 +2,15 @@ import { calculateInvestmentResults } from '../util/investment.js'
 
 export default function Results({input}) {
     const resultsData = calculateInvestmentResults(input);
-    console.log(resultsData);
-    return <p>Results...</p>
+    return <table id='result'>
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </tr>
+        </thead>
+    </table>
 }
